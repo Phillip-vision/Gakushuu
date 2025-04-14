6f0d7c72f24616e7ed86d7e6bbfe83b8400557ac
Phillip-vision <T.phillip@vision-sys.co.jp>
2025-04-14 09:50:20 +0900

    修正

--- renshuu.lua
+++ renshuu.lua
@@ -3,23 +3,21 @@ print("welcome to the random number game")
 local playAgain = "again"
 repeat
     print("choose a number")
-attempts = 0
+local attempts = 0
 local number = math.random(100)
 repeat
 attempts = attempts + 1
-    i = tonumber(io.read())
-    if i == null then
+    userInput = tonumber(io.read())
+    if userInput == nill then
      print("input error: choose a number")
-    elseif i > number then
+    elseif userInput > number then
          print("too high")
-    elseif i < number then
+    elseif userInput < number then
          print("too low")
-    elseif i == number then
+    elseif userInput == number then
          print("good job!")
-    else
-          print("whats going on")
     end
-until i == number
+until userInput == number
 print("You made " .. attempts .. " attempts. Input the word again to play once more, or enter any key to quit")
 playAgain = io.read()
 until playAgain ~= "again"
